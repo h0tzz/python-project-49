860fb1de5d401913b56b0e69a30f06d217010acf
Ilya Malashenko <ilya.malashenko.developer@gmail.com>
2022-08-23 17:32:09 +0300

    fix lint

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,7 @@
 #!/user/bin/env python3
 from brain_games.welcome_user import run
 
+
 def main():
     run()
 
--- brain_games/welcome_user.py
+++ brain_games/welcome_user.py
@@ -2,7 +2,7 @@ import prompt
 
 
 def run():
-    print(f'Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')
     user_name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(user_name))
     return user_name
